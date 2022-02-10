5d0dd8fb1d632090b49cbe6146cc1bf48f48cab5
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2022-02-10 11:11:36 -0500

    l03: complete

--- Lecture03_Bags/src/p1_course_bag/Main.java
+++ Lecture03_Bags/src/p1_course_bag/Main.java
@@ -1,7 +1,5 @@
 package p1_course_bag;
 
-import java.util.Arrays;
-
 public class Main {
 	public static void main(String[] args) {
 		CourseBag bag = new CourseBag(10);
@@ -17,36 +15,54 @@ public class Main {
 
 		System.out.println();
 
-		// will display the course just like above or null if not found
-		Course courseFound = bag.searchBy("MAT142");
-		System.out.println(courseFound);
+//		// will display the course just like above or null if not found
+//		Course courseFound = bag.searchBy("MAT142");
+//		System.out.println(courseFound);
+//
+//		System.out.println();
+//
+//		// search and remove a course from the bag (assuming it exists)
+//		System.out.println("before removal:");
+//		bag.display();
+//		bag.removeBy("PHY130");
+//		System.out.println("after removal:");
+//		bag.display();
+//
+//		System.out.println();
+//
+//		// will display the courses that match the number of credits we're looking for
+//		Course[] matchedCourses = bag.searchBy(1);
+//		System.out.println(Arrays.toString(matchedCourses));
+//
+//		System.out.println();
+//
+//		// search and remove courses from the bag (via credits)
+//		System.out.println("remove by credits:");
+//		Course[] matchedCoursesCredits = bag.removeBy(4);
+//		System.out.println(Arrays.toString(matchedCoursesCredits));
+//
+//		// change the title
+//		Course match = bag.searchBy("CSE148");
+//		if (match != null) {
+//			match.setCourseTitle("Object Oriented Programming");
+//		}
+//
+//		// change all courses with 4 credits to 3
+//		Course[] matches = bag.searchBy(4);
+//		if (matches.length > 0) {
+//			for (int i = 0; i < matches.length; i++) {
+//				matches[i].setCredits(3);
+//			}
+//		}
 
-		System.out.println();
+		// find CSE148 and change the title back to OOP, don't change the bag, only the object
+		Course cse148 = bag.searchBy("CSE148");
+		Course newCse148 = new Course(cse148.getCourseNumber(), "Object Oriented Programming", cse148.getCredits());
 
-		// search and remove a course from the bag (assuming it exists)
-		System.out.println("before removal:");
+		System.out.println("copy:");
+		System.out.println(newCse148);
+
+		System.out.println("after copy was changed:");
 		bag.display();
-		bag.removeBy("PHY130");
-		System.out.println("after removal:");
-		bag.display();
-
-		System.out.println();
-
-		// will display the courses that match the number of credits we're looking for
-		Course[] matchedCourses = bag.searchBy(1);
-		System.out.println(Arrays.toString(matchedCourses));
-		
-		System.out.println();
-		
-		// search and remove courses from the bag (via credits)
-		System.out.println("remove by credits:");
-		Course[] matchedCoursesCredits = bag.removeBy(4);
-		System.out.println(Arrays.toString(matchedCoursesCredits));
-		
-		// change the title
-		Course match = bag.searchBy("CSE148");
-		if (match != null) {
-			match.setCourseTitle("Object Oriented Programming");
-		}
 	}
 }
