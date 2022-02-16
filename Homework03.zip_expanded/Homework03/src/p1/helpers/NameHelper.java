5f11d0e2f690db90cb94f510ae0cf2333be059f1
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2022-02-16 16:32:34 -0500

    hw03: eclipsify it

--- Homework03.zip_expanded/Homework03/src/p1/helpers/NameHelper.java
+++
@@ -1,9 +0,0 @@
-package p1.helpers;
-
-import p1.objects.Name;
-
-public class NameHelper {
-    public static Name generateName(int nameLength) {
-        return new Name(Util.getRandomString(nameLength), Util.getRandomString(nameLength));
-    }
-}
--- Homework03.zip_expanded/Homework03/src/p1/helpers/StudentHelper.java
+++
@@ -1,4 +0,0 @@
-package p1.helpers;
-
-public class StudentHelper {
-}
