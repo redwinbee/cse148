0578b372d5fedbeaf133bfcca094eec1bfc49770
Edwin Bermudes <74934197+redwinbee@users.noreply.github.com>
2022-04-05 12:06:13 -0400

    hw07: implement #3

--- Homework07/src/p1/Demo.java
+++ Homework07/src/p1/Demo.java
@@ -1,9 +1,12 @@
 package p1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
+        // Q1
         ArrayList<Integer> listA = generateRandomIntegerList((int) (Math.random() * 20));
         ArrayList<Integer> listB = generateRandomIntegerList((int) (Math.random() * 20));
         ArrayList<Integer> merged = merge(listA, listB);
@@ -12,6 +15,7 @@ public class Demo {
         System.out.println("listB: " + listB);
         System.out.println("merged: " + merged);
 
+        // Q2
         ArrayList<Integer> listC = generateRandomIntegerList((int) (Math.random() * 20));
         listC.sort(Integer::compareTo);
         ArrayList<Integer> listD = generateRandomIntegerList((int) (Math.random() * 20));
@@ -21,6 +25,25 @@ public class Demo {
         System.out.println("listC: " + listD);
         System.out.println("listD: " + listC);
         System.out.println("mergeSorted: " + mergeSorted);
+
+        // Q3
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a command-separated list of sales: ");
+        String[] lineSales = scanner.nextLine().split(",");
+        ArrayList<Double> sales = new ArrayList<>();
+        for (String lineSale : lineSales) {
+            try {
+                sales.add(Double.valueOf(lineSale));
+            } catch (NumberFormatException ex) {
+                System.err.println("failed to parse sale value!");
+            }
+        }
+
+        System.out.print("enter a command-separated list of customer names: ");
+        String[] lineCustomers = scanner.nextLine().split(",");
+        ArrayList<String> customers = new ArrayList<>(Arrays.asList(lineCustomers));
+
+        System.out.printf("best customer: %s%n", nameOfBestCustomer(sales, customers));
     }
 
     public static ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b) {
@@ -74,6 +97,30 @@ public class Demo {
         }
 
         return out;
+    }
+
+    public static String nameOfBestCustomer(ArrayList<Double> sales, ArrayList<String> customers) {
+        double highestSale = sales.get(0);
+        String correspondingCustomer = customers.get(0);
+
+        if (sales.size() == 1 || customers.size() == 1) {
+            //only 1 person in the list
+            return correspondingCustomer;
+        } else {
+            for (int i = 0; i < customers.size(); i++) {
+                try {
+                    if (sales.get(i) > highestSale) {
+                        correspondingCustomer = customers.get(i);
+                        highestSale = sales.get(i);
+                    }
+                } catch (IndexOutOfBoundsException ex) {
+                    // no more sales, return the latest
+                    return correspondingCustomer;
+                }
+            }
+        }
+
+        return correspondingCustomer;
     }
 
     private static ArrayList<Integer> generateRandomIntegerList(int size) {
